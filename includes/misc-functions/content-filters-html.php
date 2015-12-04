fb4410ec4e37b5988ce95158acd5e32ce6749f94
Phil Johnston <support@mintplugins.com>
2015-12-03 23:41:53 -0500

    Include new HTML output for mobile popout

--- includes/misc-functions/content-filters-html.php
+++ includes/misc-functions/content-filters-html.php
@@ -32,16 +32,36 @@ function mp_stacks_brick_content_output_navigation($default_content_output, $mp_
 	//$menu_alignment = mp_core_get_post_meta( $post_id, 'mp_stacks_navigation_alignment', 'horizontal' );
 	$menu_alignment = 'horizontal';
 	
+	$mp_stacks_navigation_mobile_switchout_width = mp_core_get_post_meta( $post_id, 'mp_stacks_navigation_mobile_switchout_width', 600 );
+	$open_from = mp_core_get_post_meta( $post_id, 'mp_stacks_navigation_mobile_right_or_left', 'right' );
+	
 	ob_start(); 
 	
 	//If there is a menu to show
 	if (!empty( $the_menu_to_show ) ){
 		
+		//Echo the version actually shown in the Brick
+		echo '<div class="mp-stacks-navigation-toggle-button-holder" style="display:none;"><a class="mp-stacks-navigation-toggle"></a></div>';
 		echo '<div id="mp-stacks-navigation-container-' . $post_id . '" class="mp-stacks-navigation-container mp-stacks-navigation-alignment-' . $menu_alignment . '">';
 			wp_nav_menu( array( 'menu' => $the_menu_to_show, 'fallback_cb' => 'mp_stacks_navigation_fallback', 'container_class' => 'mp-stacks-navigation', ) );
 		echo '</div>';
 		
+		//Echo the version that will be used in the side popout
+		echo '<div class="mp-stacks-navigation-popout-holder mp-stacks-navigation-popout-holder-' . $open_from . '" style="display:none;">';
+			wp_nav_menu( array( 'menu' => $the_menu_to_show, 'fallback_cb' => 'mp_stacks_navigation_fallback', 'container_class' => 'mp-stacks-navigation-popout', ) );
+		echo '</div>';
+		
 	}
+	
+	//Enable Media Queries for JS
+	 wp_enqueue_script( 'mp_stacks_navigation_enquire', plugins_url( '/js/enquire.min.js', dirname( __FILE__ ) ) );
+	 
+	 //MP Menu JS
+	 wp_enqueue_script( 'mp_stacks_navigation_js', plugins_url( '/js/mp-stacks-navigation.js', dirname( __FILE__ ) ), array( 'jquery', 'mp_stacks_navigation_enquire' ) );
+	 wp_localize_script( 'mp_stacks_navigation_js', 'mp_stacks_navigation_vars', array(
+ 	 	'change_at_screen_width' => $mp_stacks_navigation_mobile_switchout_width,
+		'open_from' => $open_from
+     ));
 	
 	//Return
 	return ob_get_clean();
